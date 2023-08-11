cd47a7ac04b91e80131eda902ed926214a204a2a
Erick El nino <erick.tshimbombo@yahoo.com>
2023-08-11 07:33:29 +0200

    update

--- src/Question.jsx
+++ src/Question.jsx
@@ -1,16 +1,30 @@
+import { useState } from "react";
 import { AiOutlinePlusCircle, AiOutlineMinusCircle } from "react-icons/ai";
 
 export function Question({ myquestion }) {
+  const [showInfo, setShowInfo] = useState(false);
   const { title, info } = myquestion;
+
+  function changeInfoState() {
+    return setShowInfo((data) => {
+      return !data;
+    });
+  }
+  //   console.log(showInfo);
   return (
     <article className="question">
       <header>
         <h5>{title}</h5>
+        <button className="question-btn" onClick={changeInfoState}>
+          {" "}
+          {showInfo ? (
+            <AiOutlineMinusCircle style={{ fontSize: "2rem" }} />
+          ) : (
+            <AiOutlinePlusCircle style={{ fontSize: "2rem" }} />
+          )}
+        </button>
       </header>
-      <p>{info}</p>
-      <span className="question-btn">
-        <AiOutlinePlusCircle />
-      </span>
+      {showInfo && <p>{info}</p>}
     </article>
   );
 }
